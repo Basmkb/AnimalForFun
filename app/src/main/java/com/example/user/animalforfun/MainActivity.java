88bf323c6b3f1e7ed09f58729b7f9bcd10c26f57
Basmkb <basgenz11@gmail.com>
2017-03-30 21:02:08 +0700

    Start Game Complete

--- app/src/main/java/com/example/user/animalforfun/MainActivity.java
+++ app/src/main/java/com/example/user/animalforfun/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.user.animalforfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+String nameString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.btnStart);
+        final EditText editText = (EditText) findViewById(R.id.txtName);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText.length()==0){ //ถ้า editText ว่างยังไม่กรอกชื่อ
+                    Toast.makeText(getApplicationContext(),"กรุณากรอกชื่อด้วยครับ",Toast.LENGTH_LONG).show();
+                } else {
+                    nameString = editText.getText().toString().trim();
+                    Toast.makeText(getApplicationContext(), "Hello"+nameString, Toast.LENGTH_LONG).show();
+                    Intent startIntent = new Intent(MainActivity.this, Game.class);
+                    startActivity(startIntent);
+                }
+            }
+        });
     }
 }
